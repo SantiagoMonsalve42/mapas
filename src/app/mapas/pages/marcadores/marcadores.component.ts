bc5b8363159a62c46acac215841b82048e7af6e7
santiago monsalve <santi.monsalve09@gmail.com>
2021-07-31 13:19:20 -0500

    add the function flyTo() for each marker

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,5 +1,11 @@
 import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
+
+interface marcadorColor{
+  color: string,
+  marker: mapboxgl.Marker;
+}
+
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
@@ -8,23 +14,24 @@ import * as mapboxgl from 'mapbox-gl';
     height: 100%;
     width: 100%;
   }
-  .row{
-    background-color:white;
+  .list-group{
     border-radius:5px;
     position: fixed;
-    bottom: 50px;
-    left: 50px;
-    padding: 10px;
-    z-index:999;
-    width: 400px;
+    top: 20px;
+    right: 20px;
+    z-index: 99;
+  }
+  li{
+    cursor: pointer;
   }`
   ]
 })
 export class MarcadoresComponent implements AfterViewInit {
-  
+
   @ViewChild('mapaPointers') divMapa!:ElementRef;
+  listMarkers: marcadorColor[]=[];
   mapa!: mapboxgl.Map;
-  zoomLevel: number = 12;
+  zoomLevel: number = 15;
   center: [number,number]=[-74.0692687695204,4.612287333389066];
 
 
@@ -42,6 +49,38 @@ export class MarcadoresComponent implements AfterViewInit {
       center: this.center,
       zoom: this.zoomLevel
     });
+
+    /*const marker= new mapboxgl.Marker()
+                      .setLngLat(this.center)
+                      .addTo(this.mapa);*/
+    
+
+    
+  }
+
+  randomizeColor(): string{
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    return color;
+  }
+
+  goToMarket(): void{
+
+  }
+
+  addMarket(): void{
+    const color= this.randomizeColor();
+    const newMarker = new mapboxgl
+          .Marker({
+            draggable: true,
+            color
+          }).setLngLat(this.center)
+            .addTo(this.mapa);
+    if(this.listMarkers.length < 8){
+      this.listMarkers.push({color,marker: newMarker});  
+    }else{
+      this.listMarkers.splice(7,1);
+      this.listMarkers.splice(0,0,{color,marker: newMarker});
+    }
     
   }
 }
